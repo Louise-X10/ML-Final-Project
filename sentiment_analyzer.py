a73333818a61d651e976c714177012116bd26fb9
Francisco Nguyen <63086526+nguyen-fran@users.noreply.github.com>
2022-12-07 17:07:34 -0800

    sentiment analyzer code

---
+++ sentiment_analyzer.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+sia = SentimentIntensityAnalyzer()
+
+def is_positive(tweet):
+    return 1 if sia.polarity_scores(tweet)['compound'] > 0 else 0
+
+tweets = pd.read_csv('unique_tweets.csv')
+tweet_text = [t.replace('://','//') for t in tweets['text']]
+
+print('going through tweet polarity')
+polarity = list(map(is_positive, tweet_text))
+print('done going through tweet polarity')
+
+tweets.insert(0, 'polarity', polarity)
+tweets.to_csv('unique_tweet_sentiment.csv', mode='w', index=False)
